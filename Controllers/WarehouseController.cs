efe87cea5a83a06e51828c9791dbb57516f5d96c
Tomasz Florczyk <tomasz.florczyk98@gmail.com>
2023-11-28 19:23:30 +0100

    Added some folders, upgrade .Net Core to 8.0, installed EF 8.0

---
+++ Controllers/WarehouseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WarehouseMenagementAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WarehouseController : ControllerBase
+    {
+    }
+}
---
+++ Models/Warehouse.cs
@@ -0,0 +1,8 @@
+﻿namespace WarehouseMenagementAPI.Models
+{
+    public class Warehouse
+    {
+        public int Id { get; set; }
+        public int Lane { get; set; }
+    }
+}
